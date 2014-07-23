4e2e7d1a6b7fb0e66eebd09f04eed74f13abe235
Johan Euphrosine <proppy@google.com>
2014-07-23 11:44:35 -0700

    log greetings to file

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ import webapp2
 class HelloHandler(webapp2.RequestHandler):
   def get(self):
     msg = 'hello %s\n' % self.request.headers.get('X-AppEngine-Country', 'world')
+    with open(MESSAGES, 'a') as f: f.write(msg)
     self.response.headers['Content-Type'] = 'text/plain'
     self.response.out.write(msg)
 
-app = webapp2.WSGIApplication([('/', HelloHandler)],
+class MessagesHandler(webapp2.RequestHandler):
+  def get(self):
+    with open(MESSAGES, 'r') as f:
+      for msg in f: self.response.out.write(msg)
+
+app = webapp2.WSGIApplication([('/', HelloHandler),
+                               ('/messages', MessagesHandler)],
                                debug=True)
